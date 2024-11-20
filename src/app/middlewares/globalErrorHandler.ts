903ae6a5d4cec89c7417a794dc45d83a5a2ab473
inzamulhaque <mdihalif@gmail.com>
2024-11-20 18:05:43 +0600

    create global error handler and prisma client

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,10 +1,23 @@
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 
 const globalErrorHandler = (
   error: any,
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  let statusCode: number = httpStatus.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = error.message || "Something went wrong!";
+  let err = error;
+
+  res.status(statusCode).json({
+    success,
+    message,
+    error: err,
+  });
+};
 
 export default globalErrorHandler;
