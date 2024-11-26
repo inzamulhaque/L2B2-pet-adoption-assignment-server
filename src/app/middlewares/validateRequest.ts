0c0cac9703c7fbd88902269a7ce7e21a6f59a11c
inzamulhaque <mdihalif@gmail.com>
2024-11-26 16:37:59 +0600

    create middleware for validation zod schema

---
+++ src/app/middlewares/validateRequest.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { AnyZodObject } from "zod";
+
+const validateRequest =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+      });
+
+      return next();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+export default validateRequest;
