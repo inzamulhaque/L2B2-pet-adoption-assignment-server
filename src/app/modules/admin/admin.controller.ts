a6785aea6ca61c7c30655454a4ac6c4e05cb0a8e
inzamulhaque <mdihalif@gmail.com>
2024-12-13 18:17:37 +0600

    create pick function

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -4,6 +4,8 @@ import { getUserByIdService } from "./admin.services";
 import httpStatus from "http-status";
 import sendResponse from "../../../utils/sendResponse";
 
+const getAllUser = catchAsync(async (req: Request, res: Response) => {});
+
 const getUserById = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
   const result = await getUserByIdService(id);
@@ -16,6 +18,4 @@ const getUserById = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-const getAllUser = catchAsync(async (req: Request, res: Response) => {});
-
-export { getUserById, getAllUser };
+export { getAllUser, getUserById };
